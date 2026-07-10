9c46bd88f8f51b858d9aaf639611a7b416377fa5
samee <shaikhsamee0577@gmail.com>
2026-07-11 00:28:18 +0530

    integration testing with function prop management

--- module7/src/Counter.test.tsx
+++ module7/src/Counter.test.tsx
@@ -1,4 +1,4 @@
-import { describe, it, expect } from "vitest"
+import { describe, it, expect, vi } from "vitest"
 import { Counter } from "./Counter"
 import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
@@ -13,11 +13,21 @@ describe("Counter", () => {
     it("should increment count when button is clicked", async () => {
         const user = userEvent.setup()
         render(<Counter />)
-        
+
         const Increment = screen.getByRole("button", { name: "Increment" });
         await user.click(Increment);
 
         expect(screen.getByText("count: 3")).toBeInTheDocument();
+    })
+
+    it("renders the component with the function passed as prop", async () => {
+        const onIncrementMock = vi.fn();
+        const user = userEvent.setup()
+
+        render(<Counter onIncrement={onIncrementMock} />)
+        await user.click(screen.getByRole("button", { name: "Increment" }))
+        
+        expect(onIncrementMock).toHaveBeenCalledWith(3)
     })
 
 });
--- module7/src/Counter.tsx
+++ module7/src/Counter.tsx
@@ -1,16 +1,21 @@
 import { useState } from "react"
 
-export function Counter() {
-  const [count, setCount] = useState(0)
+export function Counter({onIncrement}: {onIncrement?: (newCount: number) => void}) {
 
-  return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
-      <h1>count: {count}</h1>
-      <button onClick={increment()} style={{ padding: "20px", borderRadius: "5px" }}>Increment</button>
-    </div>
-  )
+    const [count, setCount] = useState(0)
 
-  function increment() {
-    return () => setCount((count) => count + 3)
-  }
+    function increment() {
+        const newCount = count + 3;
+        setCount(newCount);
+        if (onIncrement) {
+            onIncrement(newCount);
+        }
+    }
+    return (
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
+            <h1>count: {count}</h1>
+            <button onClick={increment} style={{ padding: "20px", borderRadius: "5px" }}>Increment</button>
+        </div>
+    )
+
 }
