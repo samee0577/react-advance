93732dfa2fcf0f9eb377b0a4de564bdb30a272d5
samee <shaikhsamee0577@gmail.com>
2026-07-31 19:08:21 +0530

    loader added

--- 1-4project/src/features/projects/component/NavBar.tsx
+++ 1-4project/src/features/projects/component/NavBar.tsx
@@ -1,28 +1,26 @@
 import { Link } from "react-router-dom"
-import { use } from "react"
-import { ProjectsContext } from "../context/projectContext"
 import "../../../index.css"
+import { useQuery } from "@tanstack/react-query"
 
 export default function Navbar() {
 
-    const context = use(ProjectsContext)
-    const state = context?.state
-
-    if (!context) {
-        throw new Error("useProject must be used within a ProjectProvider")
-    }
-    if (!state) {
-        throw new Error("state is undefined")
-    }
+    const { data: projects, isLoading } = useQuery({
+        queryKey: ["projects"],
+        queryFn: async () => await fetch("http://localhost:3001/api/projects").then(res => res.json())
+    })
 
     return (
-        <nav style={{margin:"5px"}}>
+        <nav style={{ margin: "5px" }}>
             <div><h1>DEVBOARD</h1></div>
             <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
-                <Link className="buttonStyle"  to="/">Dashboard</Link>
+                <Link className="buttonStyle" to="/">Dashboard</Link>
                 <Link className="buttonStyle" to="/newProject">+ New Project</Link>
             </div>
-            <span>Total Projects: {state.projects.length}</span>
+            {isLoading ? (
+                <span>Total Projects: <span className="skeleton-badge"></span></span>
+            ) : (
+                <span>Total Projects: {projects?.length || 0}</span>
+            )}
         </nav>
     )
 
